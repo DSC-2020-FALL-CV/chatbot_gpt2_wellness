6ba17735400fa22d730cbb09b735fb3533eb06af
GoonLee <auteur.kor@gmail.com>
2021-02-11 00:20:14 +0900

    update train.py

--- backend/train.py
+++ backend/train.py
@@ -33,46 +33,34 @@ try:
 except:
 	print("Unable to find checkpoint. If you saved checkpoint before, check checkpoint_path")
 
-try:
-	# Dataset
-	dataset = CharDataset(datafile_path, vocab, sentencepieceTokenizer)
-	dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
-	
-	# Tokenizer
-	tok_path = get_tokenizer()
-	sentencepieceTokenizer = SentencepieceTokenizer(tok_path)
+# Dataset
+dataset = CharDataset(datafile_path, vocab, sentencepieceTokenizer)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
 
-	# Optimizer
-	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# Tokenizer
+tok_path = get_tokenizer()
+sentencepieceTokenizer = SentencepieceTokenizer(tok_path)
+# Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+# Train loop
+for epoch in range(epochs):
+	for data in dataloader:
+		optimizer.zero_grad()
+		data = torch.stack(data)
+		data = data.transpose(1,0)
 
-	# Train loop
-	for epoch in range(epochs):
+		data = data.to(device)
+		moedl = model.to(device)
 
-		for data in dataloader:
+		outputs = model(data, labels=data)
+		loss, logits = outputs[:2]
+		loss = loss.to(device)
 
-			optimizer.zero_grad()
+		loss.backward()
 
-			data = torch.stack(data)
-			data = data.transpose(1,0)
-	
-			data = data.to(device)
-			moedl = model.to(device)
-	
-			outputs = model(data, labels=data)
-			loss, logits = outputs[:2]
-
-			loss = loss.to(device)
-	
-			loss.backward()
-	
-			optimizer.step()
-
-		if epoch % 2 == 0:
-			print('epoch no.{0} loss = {1:.4f}'.format(epoch, loss))
-
-	# Save
-	model.to('cpu')
-	torch.save(model.state_dict(), checkpoint_path)
-
-except:
-	print("Unable to find data files.")
+		optimizer.step()
+	if epoch % 2 == 0:
+		print('epoch no.{0} loss = {1:.4f}'.format(epoch, loss))
+# Save
+model.to('cpu')
+torch.save(model.state_dict(), checkpoint_path)
